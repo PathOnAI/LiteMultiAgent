7974930af89c3910d60c4a2c78a4990062482640
Tata0703 <danqing.zhang.personal@gmail.com>
2024-06-29 23:10:49 -0700

    update files

--- scratch/real_multi_agent/group_chat.py
+++ scratch/real_multi_agent/group_chat.py
@@ -1,3 +1,5 @@
 # different LLM
 # different agent framework
 # for loop to start the chat
+# https://github.com/microsoft/autogen/blob/main/notebook/agentchat_groupchat.ipynb
+# user_proxy, code, pm
