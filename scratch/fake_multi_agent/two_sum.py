b787b577b92ed1f1bda03a97ff5775ed5b930c7c
Tata0703 <danqing.zhang.personal@gmail.com>
2024-07-23 11:04:28 -0700

    reorg

--- scratch/fake_multi_agent/two_sum.py
+++
@@ -1,7 +0,0 @@
-def two_sum(nums, target):
-    hash_map = {}
-    for idx, num in enumerate(nums):
-        complement = target - num
-        if complement in hash_map:
-            return [hash_map[complement], idx]
-        hash_map[num] = idx
